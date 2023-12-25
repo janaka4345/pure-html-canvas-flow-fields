f8f13f21855b869b3f17ac7da26259a96541add9
banuka <banukaramanayaka76@gmail.com>
2023-12-25 13:59:56 +0530

    feat: snake line trace try1

--- src/components/draw.js
+++ src/components/draw.js
@@ -1,47 +1,46 @@
-let line = [];
+let line = [
+  {
+    startX: Math.random() * 700,
+    startY: Math.random() * 700,
+    endX: Math.random() * 700,
+    endY: Math.random() * 700,
+    color: `hsl(${Math.random() * 360 + 1},${Math.random() * 100 + 1}%,${
+      Math.random() * 100 + 1
+    }%`,
+  },
+];
 let count = 0;
 export function draw(ctx) {
-  // Your drawing logic her
-  if (count % 10 === 0) {
-    line = [];
-    for (let index = 0; index < 50; index++) {
-      const oneline = {
-        startX: Math.random() * 700,
-        startY: Math.random() * 700,
-        endX: Math.random() * 700,
-        endY: Math.random() * 700,
-      };
-      line.push(oneline);
-    }
-    for (let index = 0; index < 50; index++) {
-      const oneline = {
-        startX: Math.random() * 700,
-        startY: Math.random() * 700,
-        endX: Math.random() * 700,
-        endY: Math.random() * 700,
-      };
-      line.push(oneline);
-    }
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.lineWidth = 4;
-    line.forEach((line) => {
-      ctx.strokeStyle = `hsl(
-        ${Math.random() * 360 + 1},
-        ${Math.random() * 100 + 1}%,
-        ${Math.random() * 100 + 1}%
-        `;
-      ctx.beginPath();
-      ctx.moveTo(line.startX, line.startY);
-      ctx.lineTo(line.endX, line.endY);
-      ctx.stroke();
-      line.startX = line.endX;
-      line.startY = line.endY;
-      line.endX = Math.random() * 700;
-      line.endY = Math.random() * 700;
-    });
+  // Your drawing logic here
+  if (count % 100 === 0) {
   }
-  // if (count % 50 === 0) {
-  //
-  // }
-  count += 1;
+  console.log(line);
+  if (line.length > 4) {
+    line.shift();
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+  ctx.lineWidth = 4;
+  line.forEach((line) => {
+    // ctx.save();
+    ctx.beginPath();
+    ctx.strokeStyle = line.color;
+    ctx.moveTo(line.startX, line.startY);
+    ctx.lineTo(
+      line.startX + line.endX * (count % 2) * 0.5,
+      line.startY + line.endY * (count % 2) * 0.5
+    );
+    ctx.stroke();
+    // ctx.restore();
+  });
+  line.push({
+    startX: line[line.length - 1].endX,
+    startY: line[line.length - 1].endY,
+    endX: Math.random() * 700,
+    endY: Math.random() * 700,
+    color: `hsl(${Math.random() * 360 + 1},${Math.random() * 100 + 1}%,${
+      Math.random() * 100 + 1
+    }%`,
+  });
 }
+
+count += 1;
