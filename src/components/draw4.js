b021e40b5cf8f63a3f57e249d1f325d474c474de
banuka <banukaramanayaka76@gmail.com>
2023-12-26 14:13:31 +0530

    feat: snake try first

--- src/components/draw4.js
+++ src/components/draw4.js
@@ -1,17 +1,17 @@
 let line = [];
 let count = 0;
 for (let i = 0; i < 1; i++) {
-  // const startX = Math.random() * 700;
-  // const startY = Math.random() * 700;
-  // const endX = Math.random() * 700;
-  // const endY = Math.random() * 700;
-  const startX = 10;
-  const startY = 10;
-  const endX = 400;
-  const endY = 400;
+  const startX = Math.random() * 100;
+  const startY = Math.random() * 100;
+  const endX = Math.random() * 700;
+  const endY = Math.random() * 700;
+  // const startX = 10;
+  // const startY = 10;
+  // const endX = 400;
+  // const endY = 400;
   const speedX = 10;
   const speedY = 5;
-  const lineLength = 10;
+  const lineLength = 1;
   const color = `hsl(${Math.random() * 360 + 1},${Math.random() * 100 + 1}%,${
     Math.random() * 50 + 1
   }%`;
@@ -19,7 +19,6 @@ for (let i = 0; i < 1; i++) {
 }
 export function draw4(ctx) {
   //   console.log(line);
-  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
   ctx.lineWidth = 4;
   ctx.strokeStyle = line[0].color;
@@ -28,22 +27,24 @@ export function draw4(ctx) {
     line[0].startX + line[0].endX * (count % 100) * 0.01 + line[0].lineLength,
     line[0].startY + line[0].endY * (count % 100) * 0.01 + line[0].lineLength
   );
-  ctx.lineTo(
-    line[0].endX * (count % 100) * 0.01 + 10,
-    line[0].endY * (count % 100) * 0.01 + 5
-  );
-  ctx.lineTo(
-    line[0].endX * (count % 100) * 0.01 + 50,
-    line[0].endY * (count % 100) * 0.01 + 20
-  );
-  ctx.lineTo(
-    line[0].endX * (count % 100) * 0.01 + 20,
-    line[0].endY * (count % 100) * 0.01 + 50
-  );
-  ctx.lineTo(
-    line[0].endX * (count % 100) * 0.01 + 100,
-    line[0].endY * (count % 100) * 0.01 + 10
-  );
+  if (count % 10 === 0) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    for (let index = 0; index < 3; index++) {
+      ctx.lineTo(
+        line[0].endX * (count % 100) * 0.01 + Math.random() * 50,
+        line[0].endY * (count % 100) * 0.01 + Math.random() * 50
+      );
+    }
+    // ctx.lineTo(
+    //   line[0].startX +
+    //     line[0].endX * ((count + 1) % 100) * 0.01 +
+    //     line[0].lineLength,
+    //   line[0].startY +
+    //     line[0].endY * ((count + 1) % 100) * 0.01 +
+    //     line[0].lineLength
+    // );
+  }
+
   ctx.stroke();
 
   count += 1;
